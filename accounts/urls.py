086cc7cae7ad1f1fe65f150ee5f79d9de0f589a1
Tomeczekqq <jarnutek@gmail.com>
2018-12-13 19:27:55 +0100

    Added account panel/password change

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name = "accounts"
 
 urlpatterns = [
+    path('password', views.changepass_view, name="changepass"),
+    path('myprofile', views.myprofile_view, name="myprofile"),
     path('login', views.login_view, name="login"),
     path('register', views.register_view, name="register"),
     path('logout', views.logout_view, name="logout"),
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, PasswordChangeForm
+from django.contrib.auth import login, logout, update_session_auth_hash
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 
 def login_view(request):
@@ -36,3 +37,28 @@ def logout_view(request):
     if request.method == 'GET':
         logout(request)
         return redirect('core:index')
+
+
+@login_required
+def myprofile_view(request):
+    user = User.objects.get(username=request.user)
+    context = {
+        'user': user
+    }
+    return render(request, "accounts/myprofile.html", context)
+
+
+@login_required
+def changepass_view(request):
+    if request.method == "POST":
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            redirect('accounts:myprofile')
+    else:
+        form = PasswordChangeForm(request.user)
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/changepass.html', context)
